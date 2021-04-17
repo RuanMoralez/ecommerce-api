3484d36a88093bce75c5c5b6eddbb45a64593aab
andersonmoralez <anderson.moralez@hotmail.com>
2021-04-17 17:48:57 -0300

    add column last_name

--- api/app/Http/Controllers/Api/UserController.php
+++ api/app/Http/Controllers/Api/UserController.php
@@ -4,64 +4,105 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use http\Env\Response;
+use Illuminate\Support\Facades\Auth;
+use Validator;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Register a User.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function register(Request $request)
     {
-        $users = User::all();
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|between:2,100',
+            'last_name' => 'required|between:2,100',
+            'email' => 'required|email|unique:users|max:50',
+            'password' => 'required|confirmed|string|min:3',
+            // 'password' => 'required|confirmed|string|min:6',
+        ]);
 
-        return response()->json($users);
+        $user = User::create(array_merge(
+                    $validator->validated(),
+                    ['password' => bcrypt($request->password)]
+                ));
+
+        return response()->json([
+            'message' => 'Successfully registered',
+            'user' => $user
+        ], 201);
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Get a JWT via given credentials.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(Request $request)
+    public function login(Request $request)
     {
-        //
+        $credentials = $request->only(['email', 'password']);
+
+        if (!$token = auth('api')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $user = auth('api')->user();
+
+        return response()->json([
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+            ],
+            'token' => $this->respondWithToken($token)
+        ]);
     }
 
     /**
-     * Display the specified resource.
+     * Log the user out (Invalidate the token).
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
+    public function logout()
     {
-        //
-    }
+        auth('api')->logout();
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
+        return response()->json(['message' => 'Successfully logged out']);
     }
-
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+     * Remove user into database
+     **/
     public function destroy($id)
     {
-        //
+        if (User::where('id', $id)->exists()) {
+            $user = User::find($id);
+            $user->delete();
+            auth('api')->logout();
+
+            return response()->json([
+                'message' => 'user deleted'
+            ], 202);
+        } else {
+            return response()->json([
+                'message' => 'user not found'
+            ], 404);
+        }
     }
+
+    /**
+     * Create a new token
+     **/
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
 }
